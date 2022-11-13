69458f52b715f3701a2adddcb1e687926eb2c0a4
Mohamed Abrar <75939387+DexTer-9@users.noreply.github.com>
2022-11-13 13:44:21 +0100

    create all components files

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
-import './index.css'
+import React from "react";
+import "./index.css";
+import About from "./components/about/About.jsx";
+import Contact from "./components/contact/Contact.jsx";
+import Experience from "./components/experience/Experience.jsx";
+import Footer from "./components/footer/Footer.jsx";
+import Header from "./components/header/Header.jsx";
+import Nav from "./components/nav/Nav.jsx";
+import Portfolio from "./components/portfolio/Portfolio.jsx";
+import Services from "./components/services/Services.jsx";
+import Testmonials from "./components/testmonials/Testmonials.jsx";
 
 function App() {
   return (
-    <div>
-      hello
-    </div>
-  )
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Testmonials />
+      <Contact />
+      <Footer />
+    </>
+  );
 }
 
-export default App
+export default App;

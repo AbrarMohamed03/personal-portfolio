8cae6e8d78cc6a78b3637c55a2d008f7e4709230
Mohamed Abrar <75939387+DexTer-9@users.noreply.github.com>
2022-11-21 21:13:08 +0100

    Header Done

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
-import './header.css'
+import React from "react";
+import Container from "react-bootstrap/Container";
+
+import CTA from "./CTA";
+import "./header.css";
+import ME from "../../assets/me.png";
+import HeaderSocials from "./HeaderSocials";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header>
+      {/* <Container> */}
+        <div className="container header__container">
+          <h5>Hello I'm</h5>
+          <h1>Mohamed Abrar</h1>
+          <h5 className="text-light">FullStack Devleloper</h5>
+          <CTA />
 
-export default Header
+          <div className="me">
+            <img src={ME} alt="me" />
+          </div>
+
+          <a href="#contact" className="scroll__down">
+            Scroll Down
+          </a>
+
+          <HeaderSocials />
+        </div>
+      {/* </Container> */}
+    </header>
+  );
+};
+
+export default Header;

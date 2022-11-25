7e3b12f389d30f18ee4aa42e5d45493d29f6def9
Mohamed Abrar <75939387+DexTer-9@users.noreply.github.com>
2022-11-25 19:18:00 +0100

    services done

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,10 +1,120 @@
-import React from 'react'
-import './services.css'
+import React from "react";
+import "./services.css";
+import { BiCheck } from "react-icons/bi";
 
 const Services = () => {
   return (
-    <div>Services</div>
-  )
-}
+    <section id="services">
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
 
-export default Services
+      <div className="container services__container">
+{/* ======================== UI/UX Design =============================== */}
+        <article className="service">
+          <div className="service__head">
+            <h3>UI/UX Design</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing</p>
+            </li>
+
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing</p>
+            </li>
+
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing</p>
+            </li>
+
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing</p>
+            </li>
+
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing</p>
+            </li>
+          </ul>
+        </article>
+
+{/* ======================== WEB DEVELOPMENT =============================== */}
+        <article className="service">
+          <div className="service__head">
+            <h3>Web Development</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing</p>
+            </li>
+
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing</p>
+            </li>
+
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing</p>
+            </li>
+
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing</p>
+            </li>
+
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing</p>
+            </li>
+
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing</p>
+            </li>
+          </ul>
+        </article>
+
+{/* ======================== Content Creations =============================== */}
+        <article className="service">
+          <div className="service__head">
+            <h3>Content Creations</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing</p>
+            </li>
+
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing</p>
+            </li>
+
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing</p>
+            </li>
+
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing</p>
+            </li>
+
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing</p>
+            </li>
+          </ul>
+        </article>
+      </div>
+    </section>
+  );
+};
+
+export default Services;

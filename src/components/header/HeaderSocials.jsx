83a8dab7e4e965653779c6be7747ed8ec4970aa6
Mohamed Abrar <75939387+DexTer-9@users.noreply.github.com>
2022-11-26 12:40:26 +0100

    Portfolio done

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -5,8 +5,8 @@ import {BsGithub} from 'react-icons/bs'
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href="https://github.com/DexTer-9" ><BsLinkedin/></a>
-        <a href="https://www.linkedin.com/in/mohamed-abrar-789399236/" ><BsGithub/></a>
+        <a href="https://github.com/DexTer-9" rel='noreferrer' target='_blank'><BsLinkedin/></a>
+        <a href="https://www.linkedin.com/in/mohamed-abrar-789399236/" rel='noreferrer' target='_blank'><BsGithub/></a>
     </div>
   )
 }
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,10 +1,61 @@
 import React from 'react'
 import './portfolio.css'
+import {SiGithub} from 'react-icons/si'
+import {CgMediaLive} from 'react-icons/cg'
+import IMG1 from '../../assets/portfolio1.jpg'
+
+const projects = [
+  {
+    id:1,
+    image:IMG1,
+    title:'This is a project1 name',
+    github:'https://github.com/',
+    demo:'https://facebook.com/'
+  },
+  {
+    id:2,
+    image:IMG1,
+    title:'This is a project2 name',
+    github:'https://github.com/',
+    demo:'https://facebook.com/'
+  },
+  {
+    id:3,
+    image:IMG1,
+    title:'This is a project3 name',
+    github:'https://github.com/',
+    demo:'https://facebook.com/'
+  },
+]
 
 const Portfolio = () => {
   return (
-    <section className='portfolio'>
-      
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+
+       {
+        projects.map(({id, image, title, github, demo}) => {
+          return (
+            <article key={id} className='portfolio__item'>
+            <div className="portfolio__item-image">
+              <img src={image} alt={title} />
+            </div>
+            <h3>{title}</h3>
+            <div className="portfolio__item-cta">
+              <a href={github} 
+              className="btn" rel='noreferrer' target='_blank' title='Code surce on github'><SiGithub/></a>
+              <a href={demo} className="btn btn-primary" rel='noreferrer' target='_blank' title='Live demo'><CgMediaLive/></a>
+            </div>
+          </article>
+          )
+        })
+       }
+
+        
+      </div>
     </section>
   )
 }

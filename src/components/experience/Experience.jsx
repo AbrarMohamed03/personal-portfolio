d1a269c335982ad0f7a1ef75e172fc0f2c94adbd
Mohamed Abrar <75939387+DexTer-9@users.noreply.github.com>
2022-11-26 12:56:08 +0100

    using map method in experience

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,65 +1,88 @@
 import React from "react";
 import "./experience.css";
 import { BsPatchCheckFill } from "react-icons/bs";
+const front = [
+  {
+    id: 1,
+    lang: "HTML",
+    lvl: "Experienced",
+  },
+  {
+    id: 2,
+    lang: "CSS",
+    lvl: "Intermediate",
+  },
+  {
+    id: 3,
+    lang: "JavaScript",
+    lvl: "Intermediate",
+  },
+  {
+    id: 4,
+    lang: "Bootstrap",
+    lvl: "Experienced",
+  },
+  {
+    id: 5,
+    lang: "ReactJS",
+    lvl: "Intermediate",
+  },
+  {
+    id: 6,
+    lang: "Tailwind",
+    lvl: "Intermediate",
+  },
+];
+const back = [
+  {
+    id: 1,
+    lang: "PHP",
+    lvl: "Experienced",
+  },
+  {
+    id: 2,
+    lang: "Python",
+    lvl: "Intermediate",
+  },
+  {
+    id: 3,
+    lang: "MongoDB",
+    lvl: "Intermediate",
+  },
+  {
+    id: 4,
+    lang: "MySQL",
+    lvl: "Experienced",
+  },
+  {
+    id: 5,
+    lang: "SQLServer",
+    lvl: "Intermediate",
+  },
+];
 
 const Experience = () => {
   return (
     <section id="experience">
       <h5>What Skills I Have </h5>
       <h2>My Experience</h2>
-
       <div className="container experience__container">
+        
         {/* ================== FrontEnd Development ===================== */}
         <div className="experience__frontend">
           <h3>FrontEnd Development</h3>
           <div className="experience__content">
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4>HTML</h4>
-                <small className="text-light">Experienced</small>
-              </div>
-            </article>
-
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4>CSS</h4>
-                <small className="text-light">Intermediate</small>
-              </div>
-            </article>
-
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4>JavaScript</h4>
-                <small className="text-light">Intermediate</small>
-              </div>
-            </article>
-
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4>Bootstrap</h4>
-                <small className="text-light">Experienced</small>
-              </div>
-            </article>
-
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4>React</h4>
-                <small className="text-light">Intermediate</small>
-              </div>
-            </article>
-
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4>Tailwind</h4>
-                <small className="text-light">Intermediate</small>
-              </div>
-            </article>
+            {front.map(({ id, lang, lvl }) => {
+              return (
+                <article key={id} className="experience__details">
+                  <BsPatchCheckFill className="experience__details-icon" />
+                  <div>
+                    <h4>{lang}</h4>
+                    <small className="text-light">{lvl}</small>
+                  </div>
+                </article>
+              );
+            })}
           </div>
         </div>
 
@@ -67,47 +90,17 @@ const Experience = () => {
         <div className="experience__backend">
           <h3>BackEnd Development</h3>
           <div className="experience__content">
-
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4>PHP</h4>
-                <small className="text-light">Experienced</small>
-              </div>
-            </article>
-
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4>Python</h4>
-                <small className="text-light">Intermediate</small>
-              </div>
-            </article>
-
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4>MongoDB</h4>
-                <small className="text-light">Intermediate</small>
-              </div>
-            </article>
-
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4>MySQL</h4>
-                <small className="text-light">Experienced</small>
-              </div>
-            </article>
-
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4>SQLServer</h4>
-                <small className="text-light">Intermediate</small>
-              </div>
-            </article>
-
+            {back.map(({ id, lang, lvl }) => {
+              return (
+                <article key={id} className="experience__details">
+                  <BsPatchCheckFill className="experience__details-icon" />
+                  <div>
+                    <h4>{lang}</h4>
+                    <small className="text-light">{lvl}</small>
+                  </div>
+                </article>
+              );
+            })}
           </div>
         </div>
       </div>

2cb1a980f2a801d4f60b317b577b8796dea5133e
Mohamed Abrar <75939387+DexTer-9@users.noreply.github.com>
2022-11-28 16:46:54 +0100

    footer done (portfolio doneeeeeeeeeeeeeeeeeee)

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 import './footer.css'
+import {FaFacebook} from 'react-icons/fa'
+import {FiInstagram} from 'react-icons/fi'
+import {IoLogoTwitter} from 'react-icons/io'
+import {AiOutlineCopyrightCircle} from 'react-icons/ai'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#home" className="footer__logo">Mohamed</a>
+
+      <ul className="permalinks">
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#expeirience">Expeirience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://www.facebook.com/m0hamed.abrar"><FaFacebook/></a>
+        <a href="https://www.instagram.com/mohamed._.abrar/?next=%2F&hl=en"><FiInstagram/></a>
+        <a href="https://twitter.com/meddAbrar"><IoLogoTwitter/></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small><AiOutlineCopyrightCircle className='footer__copyright-icon'/> Mohamed Abrar. All rights reserved.</small>
+      </div>
+    </footer>
   )
 }
 

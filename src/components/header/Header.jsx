ecda15429d5fb382aaa68143df1f6ce71cd23869
Mohamed Abrar <75939387+DexTer-9@users.noreply.github.com>
2023-02-06 21:43:38 +0100

    .

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 import CTA from "./CTA";
 import "./header.css";
-import ME from "../../assets/me.png";
+// import ME from "../../assets/me.png";
 import HeaderSocials from "./HeaderSocials";
 
 const Header = () => {
@@ -18,9 +18,9 @@ const Header = () => {
 
         <CTA />
 
-        <div className="me">
+        {/* <div className="me">
           <img src={ME} alt="me" />
-        </div>
+        </div> */}
 
         <a href="#contact" className="scroll__down">
           Scroll Down
